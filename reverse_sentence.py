c11a6a57bc3343805cfa4e3ccce5c8bc20fd6f7c
MyronCodeBase <32783137+MyronCodeBase@users.noreply.github.com>
2017-11-30 07:09:35 -0500

    Update reverse_sentence.py

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -1,5 +1,7 @@
 """
-Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order
+Write a program (using functions!) that asks the user for a long string 
+containing multiple words. Print back to the user the same string, 
+except with the words in backwards order
 """
 
 
